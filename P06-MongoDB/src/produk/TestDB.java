5a9ba577c7414ff81872c050abd9002311cfcaf8
Zaerina Eriyadi <55585494+zaerinae@users.noreply.github.com>
2020-04-10 19:25:16 +0700

    Add files via upload

---
+++ P06-MongoDB/src/produk/TestDB.java
@@ -0,0 +1,93 @@
+
+package produk;
+
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoCursor;
+import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.MongoIterable;
+import static com.mongodb.client.model.Filters.eq;
+import com.mongodb.client.result.UpdateResult;
+import java.util.Date;
+import org.bson.Document;
+import org.bson.types.ObjectId;
+
+/**
+ *
+ * @author zaerina
+ */
+public class TestDB {
+    public static void main(String[] args) {
+        try {
+            //koneksi database mangoDB
+            MongoDatabase database = Koneksi.sambungDB();
+            
+            //melihat daftar koneksi (tabel)
+            System.out.println("=======================");
+            System.out.println("Daftar Tabel Dalam Database");
+            MongoIterable<String> tables = database.listCollectionNames();
+            for (String name : tables) {
+                System.out.println(name);
+            }
+            
+            //menambah data
+            System.out.println("========================");
+            System.out.println("Menambah data");
+            MongoCollection<Document> col = database.getCollection("produk");
+            Document doc = new Document();
+            doc.put("nama", "Printer InkJet");
+            doc.put("harga", 1204000);
+            doc.put("tanggal", new Date());
+            col.insertOne(doc);
+            System.out.println("Data telah disimpan dalam koleksi");
+            
+            //mendapatkan _id dari dokumen yang baru saja diinsert
+            ObjectId id = new ObjectId(doc.get("_id").toString());
+            
+            //melihat/menampilkan data
+            System.out.println("=========================");
+            System.out.println("Data dalam koleksi produk");
+            MongoCursor<Document> cursor = col.find().iterator();
+            while (cursor.hasNext()) {
+                System.out.println(cursor.next().toJson());
+                
+            }
+            
+            //mencari dokumen berdasarkan id
+            Document myDoc = col.find(eq("_id", id)).first();
+            System.out.println("==========================");
+            System.out.println("Pencarian data berdasarkan id : "+id);
+            System.out.println(myDoc.toJson());
+            
+            //mengedit data
+            Document docs = new Document();
+            docs.put("nama", "Canon");
+            Document doc_edit = new Document("$set", docs);
+            UpdateResult hasil_edit = col.updateOne(eq("_id", id), doc_edit);
+            System.out.println(hasil_edit.getModifiedCount());
+            
+            //meliat/menampilkan data
+            System.out.println("=========================");
+            System.out.println("Data dalam koleksi produk");
+            cursor = col.find().iterator();
+            while (cursor.hasNext()) {
+                System.out.println(cursor.next().toJson());
+                
+            }
+            
+            //menghapus data
+            col.deleteOne(eq("_id",id));
+            //melihat/menampilkan data
+            System.out.println("=========================");
+            System.out.println("Data dalam koleksi produk");
+            cursor = col.find().iterator();
+            while (cursor.hasNext()) {
+                System.out.println(cursor.next().toJson());
+                
+            
+            }
+        } catch (Exception e) {
+            System.out.println("Error: "+e.getMessage());
+        }
+    }
+    
+}
